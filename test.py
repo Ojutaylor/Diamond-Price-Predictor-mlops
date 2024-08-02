5fe01847c0d18287fd7bc1281510a9bd8f91c48e
Tayloroju <oderataylor@gmail.com>
2024-08-02 17:54:03 +0300

    fridaycommit

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ from src.logger.logging import logging
 logging.info("this is myb testing")
 
 
-logging.info(" this my second tesgting")
-logging.info(" this my second tesgting")
+logging.info(" this my second testing")
+logging.info(" this my second testing")
 
-logging.info(" this my second tesgting")
-logging.info(" this my second tesgting")
-logging.info(" this my second tesgting")
+logging.info(" this my second testing")
+logging.info(" this my second testing")
+logging.info(" this my second testing")
